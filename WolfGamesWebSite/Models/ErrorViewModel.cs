8e5874ea9d879b7146dce0e0143491ec12a6906d
Dain Reinhart <dain@wolfgamesllc.com>
2017-11-05 06:41:03 -0500

    Add project files.

---
+++ WolfGamesWebSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WolfGamesWebSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
