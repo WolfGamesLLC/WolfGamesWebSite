4a82b6e955514a898bee5638aec110d58c640f7c
Dain Reinhart <dain@wolfgamesllc.com>
2017-11-11 09:43:00 -0500

    Enabled SSL and added documentation to startup.cs

---
+++ WolfGamesWebSite/NamespaceDoc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WolfGamesWebSite
+{
+    /// <summary>
+    /// The <see cref="WolfGamesWebSite"/> namespace contains the 
+    /// classes for the Wolf Games site
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGenerated]
+    public class NamespaceDoc
+    {
+    }
+}
--- WolfGamesWebSite/Startup.cs
+++ WolfGamesWebSite/Startup.cs
@@ -11,21 +11,42 @@ using Microsoft.Extensions.DependencyInjection;
 using WolfGamesWebSite.Data;
 using WolfGamesWebSite.Models;
 using WolfGamesWebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
 
 namespace WolfGamesWebSite
 {
+    /// <summary>
+    /// Default startup class for the WG site app
+    /// </summary>
     public class Startup
     {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="configuration">Application configuration</param>
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        /// <summary>
+        /// Configuration accessor
+        /// </summary>
         public IConfiguration Configuration { get; }
 
+        /// <summary>
+        /// Configure application services
+        /// </summary>
+        /// <param name="services">Collection of app services</param>
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<MvcOptions>(options =>
+            {
+                options.Filters.Add(new RequireHttpsAttribute());
+            });
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
@@ -39,9 +60,19 @@ namespace WolfGamesWebSite
             services.AddMvc();
         }
 
+        /// <summary>
+        /// Configure the HTTP request pipeline
+        /// </summary>
+        /// <param name="app">app's pipeline configuration object</param>
+        /// <param name="env">app's web hosting environment</param>
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            var options = new RewriteOptions()
+               .AddRedirectToHttps();
+
+            app.UseRewriter(options);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
