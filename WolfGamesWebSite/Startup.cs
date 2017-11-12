5550221c0c386e98ea2cecddc31e5d6190b1fb93
Dain Reinhart <dain@wolfgamesllc.com>
2017-11-12 08:26:55 -0500

    Enabled login with facebook

--- WolfGamesWebSite/Startup.cs
+++ WolfGamesWebSite/Startup.cs
@@ -60,6 +60,12 @@ namespace WolfGamesWebSite
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
 
+            services.AddAuthentication().AddFacebook(facebookOptions =>
+            {
+                facebookOptions.AppId = Configuration["Authentication:Facebook:AppId"];
+                facebookOptions.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
+            });
+
             services.AddMvc();
 
             services.Configure<AuthMessageSenderOptions>(Configuration);
