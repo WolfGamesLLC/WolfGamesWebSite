d65bce14d693a498aa7afbef9ebf0b5236a01817
Dain Reinhart <dain@wolfgamesllc.com>
2017-11-18 10:20:18 -0500

    Added reference to WGSystem Added ExternalLoginViewModel tests

---
+++ WolfGamesWebSite.XUnitTestSuite/Models/AccountViewModels/NamespaceDoc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WolfGamesWebSite.XUnitTestSuite.Models.AccountViewModels
+{
+    /// <summary>
+    /// The <see cref="WolfGamesWebSite.XUnitTestSuite.Models.AccountViewModels"/> namespace contains the 
+    /// classes for the testing of Wolf Games site account view model objects
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGenerated]
+    class NamespaceDoc
+    {
+    }
+}
