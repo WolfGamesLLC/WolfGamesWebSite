e0c5542be077cec1bec1c3c0249b7d611b7b9f65
Dain Reinhart <dain@wolfgamesllc.com>
2017-11-19 11:54:44 -0500

    Updated the contact page Updated the about page

--- WolfGamesWebSite.XUnitTestSuite/HomeControllerShould.cs
+++ WolfGamesWebSite.XUnitTestSuite/HomeControllerShould.cs
@@ -6,38 +6,124 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WolfGamesWebSite.XUnitTestSuite
 {
+
+    /// <summary>
+    /// Test suite for the HomeControllerMessage object
+    /// </summary>
+    public class HomeControllerMessageShould
+    {
+        /// <summary>
+        /// The default constructor should set message to text
+        /// </summary>
+        [Fact]
+        public void HomeControllerMessagesTest()
+        {
+            string text = "test text";
+            Assert.Equal(text, new HomeControllerMessages(text).Message);
+        }
+
+        /// <summary>
+        /// The About method should return about text
+        /// </summary>
+        [Fact]
+        public void AboutTest()
+        {
+            Assert.Equal("About Wolf Games.", HomeControllerMessages.About());
+        }
+
+        /// <summary>
+        /// The Contact method should return contact text
+        /// </summary>
+        [Fact]
+        public void ContactTest()
+        {
+            Assert.Equal("We love to hear from you.", HomeControllerMessages.Contact());
+        }
+
+        /// <summary>
+        /// The Error method should return error text
+        /// </summary>
+        [Fact]
+        public void ErrorTest()
+        {
+            Assert.Equal("~/Views/Shared/Error.cshtml", HomeControllerMessages.Error());
+        }
+
+        /// <summary>
+        /// The ThankYou method should return thank you text
+        /// </summary>
+        [Fact]
+        public void ThankYouTest()
+        {
+            Assert.Equal("Thank you from all of us at Wolf Games.", HomeControllerMessages.ThankYou());
+        }
+
+        /// <summary>
+        /// The DevCorner method should return dev corner intro text
+        /// </summary>
+        [Fact]
+        public void DevCornerTest()
+        {
+            Assert.Equal("Welcome to Wolf Games' dev corner.", HomeControllerMessages.DevCorner());
+        }
+    }
+
     /// <summary>
     /// Test suite for the home controller
     /// </summary>
     public class HomeControllerShould
     {
+        HomeController Controller;
+        ViewResult Result;
+
         /// <summary>
-        /// The default constructor should create a <see cref="HomeController">home controller</see>
+        /// The test initializer for the suite
         /// </summary>
-        [Fact]
-        public void CreateHomeController()
+        public HomeControllerShould()
         {
-            Assert.IsType<HomeController>(new HomeController());
+            Controller = new HomeController();
         }
 
         /// <summary>
-        /// Index should return a <see cref="ViewResult"/>
+        /// The index action should return a ViewResult
         /// </summary>
         [Fact]
         public void IndexReturnsViewResult()
         {
-            var result = new HomeController().Index();
-            Assert.IsType<ViewResult>(result);
+            Result = Controller.Index() as ViewResult;
+            Assert.IsType<ViewResult>(Result);
         }
 
         /// <summary>
-        /// DevCorner should return a <see cref="Microsoft.AspNetCore.Mvc.ViewResult"/>
+        /// The DevCorner action should return a ViewResult
         /// </summary>
         [Fact]
         public void DevCornerReturnsViewResult()
         {
-            var result = new HomeController().DevCorner();
-            Assert.IsType<ViewResult>(result);
+            Result = Controller.DevCorner() as ViewResult;
+            Assert.IsType<ViewResult>(Result);
+        }
+
+        /// <summary>
+        /// The about action returns a ViewResult
+        /// </summary>
+        [Fact]
+        public void AboutReturnsViewResultWithAboutMessage()
+        {
+            Result = Controller.About() as ViewResult;
+            Assert.IsType<ViewResult>(Result);
+            Assert.Equal(HomeControllerMessages.About(), Result.ViewData["Message"]);
+        }
+
+        /// <summary>
+        /// The contact action returns a ViewResult
+        /// </summary>
+        [Fact]
+        public void ContactReturnsViewResultWithAboutMessage()
+        {
+            Result = Controller.Contact() as ViewResult;
+            Assert.IsType<ViewResult>(Result);
+            Assert.Equal(HomeControllerMessages.Contact(), Result.ViewData["Message"]);
         }
     }
 }
--- WolfGamesWebSite/Controllers/HomeController.cs
+++ WolfGamesWebSite/Controllers/HomeController.cs
@@ -8,35 +8,133 @@ using WolfGamesWebSite.Models;
 
 namespace WolfGamesWebSite.Controllers
 {
+    /// <summary>
+    /// The group of constants used to contain home controller related
+    /// messages. Hopefully this will prevent the primitive obsession 
+    /// smell, at least in this case
+    /// </summary>
+    public class HomeControllerMessages
+    {
+        private string message;
+
+        /// <summary>
+        /// The Message accessor
+        /// </summary>
+        public string Message
+        {
+            get
+            {
+                return message;
+            }
+        }
+
+        /// <summary>
+        /// The default controller
+        /// </summary>
+        /// <param name="message">The text the message will be set to</param>
+        public HomeControllerMessages(string message)
+        {
+            this.message = message;
+        }
+
+        /// <summary>
+        /// The about message accessor
+        /// </summary>
+        /// <returns>The about message</returns>
+        public static string About()
+        {
+            return new HomeControllerMessages("About Wolf Games.").Message;
+        }
+
+        /// <summary>
+        /// The contact message accessor
+        /// </summary>
+        /// <returns>the contact message</returns>
+        public static string Contact()
+        {
+            return new HomeControllerMessages("We love to hear from you.").Message;
+        }
+
+        /// <summary>
+        /// The error message accessor
+        /// </summary>
+        /// <returns>the error message</returns>
+        public static string Error()
+        {
+            return new HomeControllerMessages("~/Views/Shared/Error.cshtml").Message;
+        }
+
+        /// <summary>
+        /// The thank you message accessor
+        /// </summary>
+        /// <returns>the thank you message</returns>
+        public static string ThankYou()
+        {
+            return new HomeControllerMessages("Thank you from all of us at Wolf Games.").Message;
+        }
+
+        /// <summary>
+        /// The dev corner message accessor
+        /// </summary>
+        /// <returns>the dev corner message</returns>
+        public static string DevCorner()
+        {
+            return new HomeControllerMessages("Welcome to Wolf Games' dev corner.").Message;
+        }
+    }
+
+    /// <summary>
+    /// The wolf games home page controller
+    /// </summary>
     public class HomeController : Controller
     {
+        /// <summary>
+        /// The basic Index action
+        /// </summary>
+        /// <returns>The home page view </returns>
         public IActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// The about action
+        /// </summary>
+        /// <returns>The about page view</returns>
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = HomeControllerMessages.About();
 
             return View();
         }
 
+        /// <summary>
+        /// The contact action
+        /// </summary>
+        /// <returns>The contact page view</returns>
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = HomeControllerMessages.Contact();
 
             return View();
         }
 
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
+        /// <summary>
+        /// The dev corner action
+        /// </summary>
+        /// <returns>The developer's corner page view</returns>
         public object DevCorner()
         {
             return View();
+        }
+
+        /// <summary>
+        /// The error action
+        /// </summary>
+        /// <returns>The error page view</returns>
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
