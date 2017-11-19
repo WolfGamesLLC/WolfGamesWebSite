f1ea6ca7663ae52ea0a6ab3da6aec3aca31684a6
Dain Reinhart <dain@wolfgamesllc.com>
2017-11-19 11:24:26 -0500

    Updated site.min.css Documented Program.cs

--- WolfGamesWebSite/Program.cs
+++ WolfGamesWebSite/Program.cs
@@ -10,13 +10,26 @@ using Microsoft.Extensions.Logging;
 
 namespace WolfGamesWebSite
 {
+    /// <summary>
+    /// The main program class of the Wolf Games site - created by the MS template
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// The main function of the site
+        /// </summary>
+        /// <param name="args">List of args passed to the app</param>
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
         }
 
+        /// <summary>
+        /// The static builder function microsoft generated to create the web host
+        /// used by the Wolf Games site app
+        /// </summary>
+        /// <param name="args">The command line args are passed into here</param>
+        /// <returns>An object that implements IWebHost</returns>
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
