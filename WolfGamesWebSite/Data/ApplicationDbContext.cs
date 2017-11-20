a4f085697aa4fd8651f2d3c71c6ce854004361b7
Dain Reinhart <dain@wolfgamesllc.com>
2017-11-20 11:54:57 -0500

    Added MarbleMotionModelController

--- WolfGamesWebSite/Data/ApplicationDbContext.cs
+++ WolfGamesWebSite/Data/ApplicationDbContext.cs
@@ -5,16 +5,30 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using WolfGamesWebSite.Models;
+using WolfGamesWebSite.Models.SimpleGameModels;
 
 namespace WolfGamesWebSite.Data
 {
+    /// <summary>
+    /// The main application db context
+    /// </summary>
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        /// <summary>
+        /// The default constructor for the main application db
+        /// context
+        /// </summary>
+        /// <param name="options">The db context options</param>
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
 
+        /// <summary>
+        /// The operation to perform when a model is created for the 
+        /// first time
+        /// </summary>
+        /// <param name="builder">The ModelBuilder object to use </param>
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
@@ -22,5 +36,10 @@ namespace WolfGamesWebSite.Data
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
         }
+
+        /// <summary>
+        /// The MarbleMotionModel db set
+        /// </summary>
+        public DbSet<WolfGamesWebSite.Models.SimpleGameModels.MarbleMotionModel> MarbleMotionModel { get; set; }
     }
 }
