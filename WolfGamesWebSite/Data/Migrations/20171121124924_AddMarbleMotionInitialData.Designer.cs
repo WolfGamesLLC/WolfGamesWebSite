3b28838bfacc5ab3859af70f4bf59f9df9ccfc77
Dain Reinhart <dain@wolfgamesllc.com>
2017-11-21 08:10:21 -0500

    Added Swagger support Migrated DB to include MarbleMotion table

--- WolfGamesWebSite/Data/Migrations/20171121124924_AddMarbleMotionInitialData.Designer.cs
+++ WolfGamesWebSite/Data/Migrations/20171121124924_AddMarbleMotionInitialData.Designer.cs
@@ -11,9 +11,10 @@ using WolfGamesWebSite.Data;
 namespace WolfGamesWebSite.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171121124924_AddMarbleMotionInitialData")]
+    partial class AddMarbleMotionInitialData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
