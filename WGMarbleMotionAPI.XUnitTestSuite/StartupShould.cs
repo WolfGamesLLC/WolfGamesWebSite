90e707b4cabbfb096baa82421253f357a41eb955
Dain Reinhart <dain@wolfgamesllc.com>
2017-11-24 11:38:20 -0500

    Added project dependencies and references for WGMarbleMotionAPI and its test suite Added WGMarbleMotionAPI.XUnitTestSuite.StartupShould

---
+++ WGMarbleMotionAPI.XUnitTestSuite/StartupShould.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WGMarbleMotionAPI.XUnitTestSuite
+{
+    /// <summary>
+    /// Test suite for the <see cref="Startup"/> class
+    /// </summary>
+    public class StartupShould
+    {
+    }
+}
