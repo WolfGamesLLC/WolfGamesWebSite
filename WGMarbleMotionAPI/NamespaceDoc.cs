22141bfb75f7bc25078193f18918bec3140df824
Dain Reinhart <dain@wolfgamesllc.com>
2017-11-24 09:03:50 -0500

    Added WGMarbleMotionAPI project Added WGMarbleMotionAPI.XUnitTestSuite project

---
+++ WGMarbleMotionAPI/NamespaceDoc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WGMarbleMotionAPI
+{
+    /// <summary>
+    /// The <see cref="WGMarbleMotionAPI"/> namespace contains the 
+    /// classes for the Wolf Games Marble Motion Game API
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGenerated]
+    public class NamespaceDoc
+    {
+    }
+}
