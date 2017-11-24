f5ad5e758dd40aa124dbb613ffcbcb63a00564bb
Dain Reinhart <dain@wolfgamesllc.com>
2017-11-24 11:29:56 -0500

    Added documentation to WGMarbleMotionAPI Added documentation to WGMarbleMotionAPI.XUnitTestSuite Turned off launching of browser when project is built

--- WGMarbleMotionAPI/Program.cs
+++ WGMarbleMotionAPI/Program.cs
@@ -10,13 +10,25 @@ using Microsoft.Extensions.Logging;
 
 namespace WGMarbleMotionAPI
 {
+    /// <summary>
+    /// The main Program object provided by the MS template
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// The Main function of the program
+        /// </summary>
+        /// <param name="args">An array of strings containing the command line args</param>
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
         }
 
+        /// <summary>
+        /// The builder method used to create the web service
+        /// </summary>
+        /// <param name="args">An array of strings containing the command line args</param>
+        /// <returns>An object that implements IWebHost</returns>
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
--- WGMarbleMotionAPI/Startup.cs
+++ WGMarbleMotionAPI/Startup.cs
@@ -11,21 +11,41 @@ using Microsoft.Extensions.Options;
 
 namespace WGMarbleMotionAPI
 {
+    /// <summary>
+    /// Main Startup class provided by the MS template
+    /// </summary>
     public class Startup
     {
+        /// <summary>
+        /// Default constructor provided by the MS template
+        /// </summary>
+        /// <param name="configuration">An object that implements IConfiguration</param>
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        /// <summary>
+        /// An get accessor of the Startup objects Configuration member
+        /// </summary>
         public IConfiguration Configuration { get; }
 
+        /// <summary>
+        /// The configure services method to allow services to be added to the 
+        /// MVC pipeline
+        /// </summary>
+        /// <param name="services">An object that implements IServiceCollection</param>
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
         }
 
+        /// <summary>
+        /// The configure method allows the MVC pipeline to be configured
+        /// </summary>
+        /// <param name="app">An object that implements IApplicationBuilder</param>
+        /// <param name="env">An object that implements IHostingEnvironment</param>
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
