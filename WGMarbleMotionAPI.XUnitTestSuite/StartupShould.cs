cf426746fcf5a8e68eb61eeead304af5444476ac
Dain Reinhart <dain@wolfgamesllc.com>
2017-11-26 08:53:41 -0500

    Added WolfGamesWebSite.Test.Framework project

--- WGMarbleMotionAPI.XUnitTestSuite/StartupShould.cs
+++ WGMarbleMotionAPI.XUnitTestSuite/StartupShould.cs
@@ -4,7 +4,10 @@ using Moq;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using WolfGamesWebSite.Test.Framework.Mocks;
+using WolfGamesWebSite.Test.Framework.Identifiers;
 using Xunit;
+using WGSystem.Collections.Generic;
 
 namespace WGMarbleMotionAPI.XUnitTestSuite
 {
@@ -16,14 +19,14 @@ namespace WGMarbleMotionAPI.XUnitTestSuite
         [Fact]    
         public void AddMVCToServicePipeline()
         {
+            var list = new WGGenericCollectionsFactory().CreateList<string>();
             var mockConfig = new Mock<IConfiguration>();
-            var mockServices = new Mock<IServiceCollection>();
+            var mockServices = new MockServiceCollection(list);
             var startUp = new Startup(mockConfig.Object);
 
-            startUp.ConfigureServices(mockServices.Object);
+            startUp.ConfigureServices(mockServices);
 
-            Action<ServiceDescriptor>[] elementInspectors = null;
-            Assert.Collection<ServiceDescriptor>(mockServices.Object, elementInspectors);
+            Assert.Contains<string>(MockServiceCollectionIdentifiers.MVCAdded(), list);
         }
     }
 }
---
+++ WolfGamesWebSite.Test.Framework/NamespaceDoc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WolfGamesWebSite.Test.Framework
+{
+    /// <summary>
+    /// The <see cref="WolfGamesWebSite.Test.Framework"/> namespace contains extension,
+    /// wrapper and other classes helpful for testing web site objects
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGenerated]
+    public class NamespaceDoc
+    {
+    }
+}
