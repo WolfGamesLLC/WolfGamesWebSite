33e88d2de30da17e2dec55d1c99b2d856900dd2c
Dain Reinhart <dain@wolfgamesllc.com>
2017-12-22 07:17:18 -0500

    Added PlayersController to WGMarbleMotionAPI

---
+++ WGMarbleMotionAPI.XUnitTestSuite/Controllers/PlayersControllerShould.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WGMarbleMotionAPI.Controllers;
+using WolfGamesWebSite.Common.XUnitTest.Controllers;
+using Xunit;
+
+namespace WGMarbleMotionAPI.XUnitTestSuite.Controllers
+{
+    /// <summary>
+    /// Test suite for the <see cref="PlayersController"/>
+    /// </summary>
+    public class PlayersControllerShould : BaseControllerShould<PlayersController>
+    {
+        private Mock<IUrlHelper> mockUrl;
+
+        /// <summary>
+        /// The test initializer for the suite
+        /// </summary>
+        public PlayersControllerShould()
+        {
+            Controller = new PlayersController();
+            mockUrl = new Mock<IUrlHelper>();
+            mockUrl.Setup(r => r.Link("GetPlayers",null)).Returns("http://GetPlayers");
+            Controller.Url = mockUrl.Object;
+        }
+
+        /// <summary>
+        /// The <see cref="PlayersController.GetPlayers"/> action should return a OkObjectResult
+        /// </summary>
+        [Fact]
+        public void GetPlayersReturnsOkObjectResult()
+        {
+//            Assert.IsType<OkObjectResult>(Controller.GetPlayers());
+        }
+
+        /// <summary>
+        /// The <see cref="PlayersController.GetPlayers"/> action should return a ViewResult
+        /// </summary>
+        [Fact]
+        public void GetPlayersReturnsStatusCodeValueHrefEqualGetPlayers()
+        {
+//            OkObjectResult response = Controller.GetPlayers() as OkObjectResult;
+//            var exp = new { href = "http://GetPlayers" };
+//            Assert.Equal(exp.ToString(), response.Value.ToString());
+        }
+    }
+}
