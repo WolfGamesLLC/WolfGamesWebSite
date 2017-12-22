d7a72dee6563a4417b9427ae67a7a5c30e95259a
Dain Reinhart <dain@wolfgamesllc.com>
2017-12-22 08:06:24 -0500

    Pushed common integration tests to base integration test suite

--- WolfGamesWebSite.Integration.XUnitTestSuite/MarbleMotionAPI/RootRequestShould.cs
+++ WolfGamesWebSite.Integration.XUnitTestSuite/MarbleMotionAPI/RootRequestShould.cs
@@ -12,23 +12,8 @@ namespace WolfGamesWebSite.Integration.XUnitTestSuite.MarbleMotionApi
     /// <summary>
     /// Integration Test suite for the <see cref="WGMarbleMotionApi.Controller.RootController"/>
     /// </summary>
-    public class RootRequestShould : DefaultRequestShould<WGMarbleMotionAPI.Startup>
+    public class RootRequestShould : DefaultRequestShould
     {
-        private readonly TestServer _server;
-        private readonly HttpClient _client;
-
-        /// <summary>
-        /// The route to be used in base integration tests
-        /// </summary>
-        public string Route { get; set; }
-
-        private async Task<HttpResponseMessage> Request(string route)
-        {
-            var response = await _client.GetAsync(route);
-            var responseString = await response.Content.ReadAsStringAsync();
-            return response;
-        }
-
         /// <summary>
         /// Varify a default request
         /// </summary>
@@ -40,44 +25,6 @@ namespace WolfGamesWebSite.Integration.XUnitTestSuite.MarbleMotionApi
             _client = _server.CreateClient();
 
             Route = "/";
-        }
-
-        /// <summary>
-        /// A default request should return an Ok response
-        /// </summary>
-        /// <returns>A task</returns>
-        [Fact]
-        public async Task ReturnOKResponse()
-        {
-            // Act
-            HttpResponseMessage response = await Request(Route);
-
-            // Assert
-            Assert.Equal("OK", response.ReasonPhrase);
-        }
-
-        /// <summary>
-        /// A default request should use an application/ion+json content type
-        /// </summary>
-        /// <returns>A task</returns>
-        [Fact]
-        public async Task UseIonPlusJsonContentType()
-        {
-            // Act
-            HttpResponseMessage response = await Request(Route);
-
-            // Assert
-            Assert.Equal("application/ion+json", response.Content.Headers.ContentType.MediaType);
-        }
-
-        [Fact]
-        public async Task HaveDefaultVersion()
-        {
-            // Act
-            HttpResponseMessage response = await Request(Route);
-
-            // Assert
-            Assert.Contains("1.0", response.Headers.GetValues("api-supported-versions"));
         }
     }
 }
--- WolfGamesWebSite.Test.Framework/Facts/DefaultRequestShould.cs
+++ WolfGamesWebSite.Test.Framework/Facts/DefaultRequestShould.cs
@@ -1,13 +1,69 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.TestHost;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace WolfGamesWebSite.Test.Framework.Facts
 {
     /// <summary>
     /// Base integration test suite for web apps
     /// </summary>
-    public abstract class DefaultRequestShould<T>
+    public abstract class DefaultRequestShould
     {
+        protected TestServer _server;
+        protected HttpClient _client;
+
+        /// <summary>
+        /// The route to be used in base integration tests
+        /// </summary>
+        public string Route { get; set; }
+
+        private async Task<HttpResponseMessage> Request(string route)
+        {
+            var response = await _client.GetAsync(route);
+            var responseString = await response.Content.ReadAsStringAsync();
+            return response;
+        }
+
+        /// <summary>
+        /// A default request should return an Ok response
+        /// </summary>
+        /// <returns>A task</returns>
+        [Fact]
+        public async Task ReturnOKResponse()
+        {
+            // Act
+            HttpResponseMessage response = await Request(Route);
+
+            // Assert
+            Assert.Equal("OK", response.ReasonPhrase);
+        }
+
+        /// <summary>
+        /// A default request should use an application/ion+json content type
+        /// </summary>
+        /// <returns>A task</returns>
+        [Fact]
+        public async Task UseIonPlusJsonContentType()
+        {
+            // Act
+            HttpResponseMessage response = await Request(Route);
+
+            // Assert
+            Assert.Equal("application/ion+json", response.Content.Headers.ContentType.MediaType);
+        }
+
+        [Fact]
+        public async Task HaveDefaultVersion()
+        {
+            // Act
+            HttpResponseMessage response = await Request(Route);
+
+            // Assert
+            Assert.Contains("1.0", response.Headers.GetValues("api-supported-versions"));
+        }
     }
 }
