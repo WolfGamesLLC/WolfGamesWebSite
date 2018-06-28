d1031614eee4a4f84be163f765f71dc545da6bb7
Dain Reinhart <dain@wolfgamesllc.com>
2018-06-28 08:39:09 -0400

    Added verification of user manager bridge

---
+++ WolfGamesWebSite.Common.XUnitTest/Identity/RequestUserProviderShould.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WolfGamesWebSite.Common.Identity;
+using WolfGamesWebSite.DAL.Models;
+using Xunit;
+using Moq;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace WolfGamesWebSite.Common.XUnitTest.Identity
+{
+    /// <summary>
+    /// Verify the request user provider bridge behaviors
+    /// </summary>
+    public class RequestUserProviderShould
+    {
+        /// <summary>
+        /// Verify creation
+        /// </summary>
+        [Fact(Skip = "This test really doesn't make sense until I start to verify arguments")]
+        public void CreateRequestProvider()
+        {
+            RequestUserProvider provider = new RequestUserProvider(null);
+            Assert.IsAssignableFrom<IRequestUserProvider>(provider);
+        }
+
+        /// <summary>
+        /// Verify the user can be retrieved from a principle
+        /// </summary>
+        [Fact]
+        public void GetUserAsyncWithPrinciple()
+        {
+            var _store = new Mock<IUserStore<ApplicationUser>>();
+            var manager = new UserManager<ApplicationUser>(_store.Object, null, null, null, null, null, null, null, null);
+
+            RequestUserProvider provider = new RequestUserProvider(manager);
+            ClaimsPrincipal principle = new ClaimsPrincipal();
+            var user = provider.GetUserAsync(principle);
+
+            Assert.Equal("1", user.Id.ToString());
+        }
+    }
+}
--- WolfGamesWebSite.Common/Identity/IRequestUserProvider.cs
+++ WolfGamesWebSite.Common/Identity/IRequestUserProvider.cs
@@ -3,19 +3,20 @@ using System.Collections.Generic;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using WolfGamesWebSite.DAL.Models;
 
 namespace WolfGamesWebSite.Common.Identity
 {
     /// <summary>
     /// Interface for bridgeing to the Identity UserManager class
     /// </summary>
-    public interface IRequestUserProvider<T>
+    public interface IRequestUserProvider
     {
         /// <summary>
         /// Get the user data for the specified claim principle
         /// </summary>
         /// <param name="principle">the claims principle</param>
         /// <returns>A user data object</returns>
-        Task<T> GetUserAsync(ClaimsPrincipal principle);
+        Task<ApplicationUser> GetUserAsync(ClaimsPrincipal principle);
     }
 }
